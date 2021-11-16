2572692fc58dc65699c12f55942c80591094c48b
Paulo-Dandrea <prog.dandrea@gmail.com>
2021-11-16 08:38:05 -0300

    chore: first project configurations

---
+++ src/ui/theme/global-style.ts
@@ -0,0 +1,42 @@
+import { createGlobalStyle } from "styled-components";
+
+// import colors from "./colors";
+
+export const GlobalStyle = createGlobalStyle`
+ @font-face {
+    font-family: "Sharon";
+    font-weight: 400;
+    font-style: normal;
+    src: url("/fonts/SharonSans-Regular.woff2") format("woff2"),
+      url("/fonts/SharonSans-Regular.woff") format("woff");
+  }
+
+  @font-face {
+    font-family: "Sharon";
+    font-weight: 600;
+    font-style: normal;
+    src: url("/fonts/SharonSans-Medium.woff2") format("woff2"),
+      url("/fonts/SharonSans-Medium.woff") format("woff");
+  }
+
+  @font-face {
+    
+    font-family: "Sharon"; 
+
+    
+    font-weight: 700;
+    font-style: normal;
+    src: url("/fonts/SharonSans-Bold.woff2") format("woff2"),
+      url("/fonts/SharonSans-Bold.woff") format("woff");
+  }
+
+  * {
+    font-family: Sharon, Helvetica, Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    -moz-osx-font-smoothing: grayscale;
+    text-rendering: optimizeLegibility;
+    text-size-adjust: none;
+  }
+  `;
