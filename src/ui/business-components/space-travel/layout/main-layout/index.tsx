010779daa7a337f2efa86f2704112efbf9b107b2
Paulo-Dandrea <prog.dandrea@gmail.com>
2021-11-24 09:46:28 -0300

    feat: Box, Flex and Grid styled-components

--- src/ui/business-components/space-travel/layout/main-layout/index.tsx
+++ src/ui/business-components/space-travel/layout/main-layout/index.tsx
@@ -10,12 +10,11 @@ import { SpaceTravelStyle } from '~/ui/theme/space-travel-style'
 
 import logo from 'public/space-travel/assets/shared/logo.svg'
 import { Header, Layout } from './style'
+import { Box, Flex } from '~/ui/components/utils'
 
 interface LayoutProps {
   children?: ReactNode
 }
-
-
 
 const MainLayout = (props: LayoutProps) => {
   const appTitle = `Space-Travel`
@@ -34,10 +33,14 @@ const MainLayout = (props: LayoutProps) => {
         <div>
           <Image src={logo} width="100px" height="100px" alt="Space tourism logo" />
         </div>
-        <PrimaryNavigation />
+        {/* <PrimaryNavigation /> */}
       </Header>
 
-      <div className='all-children'>{props.children}</div>
+      <div className="all-children">{props.children}</div>
+      <Flex justifyContent="space-between">
+        <Box color="white">Very</Box>
+        <Box color="white">NICE</Box>
+      </Flex>
       {/* <Link href="/">Home from Home</Link> */}
     </Layout>
   )
--- src/ui/components/utils/index.ts
+++ src/ui/components/utils/index.ts
@@ -1,1 +1,2 @@
+export { Box } from './box'
 export { Flex } from './flex'
