ba68fe2b0e34c7fb39e2d837104128810cefb422
Paulo-Dandrea <prog.dandrea@gmail.com>
2021-11-24 08:49:44 -0300

    refactor: modularize grid container

--- src/ui/business-components/space-travel/layout/main-layout/grid-container/index.ts
+++ src/ui/business-components/space-travel/layout/main-layout/grid-container/index.ts
@@ -1,14 +1,23 @@
 import styled from 'styled-components'
 
 export const GridContainer = styled.div`
-  display: grid;
-
   border: 5px solid limegreen;
+  display: grid;
+  padding-inline: 1rem;
+  place-content: center;
+  place-items: center;
+  text-align: center;
+
+  * {
+    max-width: 45ch;
+  }
+
+  gap: 10%;
 
   @media (min-width: 45em) {
     grid-template-columns: minmax(2em, 1fr) repeat(2, minmax(0, 40rem)) minmax(2em, 1fr);
 
-    column-gap: 2rem;
+    text-align: left;
 
     & > *:first-child {
       grid-column: 2;
@@ -20,4 +29,17 @@ export const GridContainer = styled.div`
       outline: 1px solid yellow;
     }
   }
+  & > *:first-child {
+    outline: 1px solid red;
+  }
+
+  & > *:last-child {
+    outline: 1px solid yellow;
+  }
+`
+
+export const GridContainerHome = styled(GridContainer)`
+  /* background-color: red; */
+  padding-bottom: 6rem;
+  align-items: end;
 `
